a87ba373bf6db911b10474ee75fa6f41ae0501e8
Amine <aminossky1@gmail.com>
2024-01-22 13:23:32 +0100

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace Srak
+{
+    partial class Srak
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Srak
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Srak";
+            this.Text = "root";
+            this.Load += new System.EventHandler(this.Srak_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Srak
+{
+    public partial class Srak : Form
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetCursorPos(int x, int y);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool mouse_event(uint dwFlags, int dx, int dy, uint dwData, UIntPtr dwExtraInfo);
+
+        const int KEYEVENTF_KEYDOWN = 0x0001;
+        const int KEYEVENTF_KEYUP = 0x0002;
+        const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+        const int MOUSEEVENTF_LEFTUP = 0x0004;
+
+        public Srak()
+        {
+            InitializeComponent();
+        }
+
+        private async void Srak_Load(object sender, EventArgs e)
+        {
+            int time = 0;
+            while (time < 10)
+            {
+                await SimulerAppuiToucheAsync(Keys.B);
+                Thread.Sleep(1000);
+                time++;
+            }
+        }
+        static Task SimulerAppuiToucheAsync(Keys touche)
+        {
+            return Task.Run(() =>
+            {
+                keybd_event((byte)touche, 0, KEYEVENTF_KEYDOWN, UIntPtr.Zero);
+                keybd_event((byte)touche, 0, KEYEVENTF_KEYUP, UIntPtr.Zero);
+            });
+        }
+
+        static void SimulerClicSouris(int x, int y)
+        {
+            SetCursorPos(x, y);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, UIntPtr.Zero);
+            mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, UIntPtr.Zero);
+        }
+    }
+}
