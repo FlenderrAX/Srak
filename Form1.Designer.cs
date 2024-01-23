14095a9450b06b17459db319d62090cf3b8e127f
Amine <aminossky1@gmail.com>
2024-01-23 18:27:53 +0100

    Added button to enable and disable and a hooked listener to check if the toggle key is pressed

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,21 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.enableAuto = new System.Windows.Forms.Button();
+            this.disableAuto = new System.Windows.Forms.Button();
+            this.clickCombobox = new System.Windows.Forms.ComboBox();
+            this.msTextbox = new System.Windows.Forms.TextBox();
+            this.msLabel = new System.Windows.Forms.Label();
+            this.clickCombo = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // enableAuto
+            // 
+            this.enableAuto.Location = new System.Drawing.Point(16, 368);
+            this.enableAuto.Name = "enableAuto";
+            this.enableAuto.Size = new System.Drawing.Size(210, 70);
+            this.enableAuto.TabIndex = 0;
+            this.enableAuto.Text = "Enable Autoclick (F6)";
+            this.enableAuto.UseVisualStyleBackColor = true;
+            // 
+            // disableAuto
+            // 
+            this.disableAuto.Enabled = false;
+            this.disableAuto.Location = new System.Drawing.Point(232, 368);
+            this.disableAuto.Name = "disableAuto";
+            this.disableAuto.Size = new System.Drawing.Size(210, 70);
+            this.disableAuto.TabIndex = 1;
+            this.disableAuto.Text = "Disable Autoclick (F6)";
+            this.disableAuto.UseVisualStyleBackColor = true;
+            // 
+            // clickCombobox
+            // 
+            this.clickCombobox.FormattingEnabled = true;
+            this.clickCombobox.Location = new System.Drawing.Point(105, 341);
+            this.clickCombobox.Name = "clickCombobox";
+            this.clickCombobox.Size = new System.Drawing.Size(121, 21);
+            this.clickCombobox.TabIndex = 2;
+            // 
+            // msTextbox
+            // 
+            this.msTextbox.Location = new System.Drawing.Point(321, 342);
+            this.msTextbox.Name = "msTextbox";
+            this.msTextbox.Size = new System.Drawing.Size(121, 20);
+            this.msTextbox.TabIndex = 3;
+            // 
+            // msLabel
+            // 
+            this.msLabel.AutoSize = true;
+            this.msLabel.Location = new System.Drawing.Point(318, 326);
+            this.msLabel.Name = "msLabel";
+            this.msLabel.Size = new System.Drawing.Size(62, 13);
+            this.msLabel.TabIndex = 4;
+            this.msLabel.Text = "Miliseconds";
+            // 
+            // clickCombo
+            // 
+            this.clickCombo.AutoSize = true;
+            this.clickCombo.Location = new System.Drawing.Point(102, 325);
+            this.clickCombo.Name = "clickCombo";
+            this.clickCombo.Size = new System.Drawing.Size(30, 13);
+            this.clickCombo.TabIndex = 5;
+            this.clickCombo.Text = "Click";
             // 
             // Srak
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(458, 450);
+            this.Controls.Add(this.clickCombo);
+            this.Controls.Add(this.msLabel);
+            this.Controls.Add(this.msTextbox);
+            this.Controls.Add(this.clickCombobox);
+            this.Controls.Add(this.disableAuto);
+            this.Controls.Add(this.enableAuto);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "Srak";
             this.Text = "root";
             this.Load += new System.EventHandler(this.Srak_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button enableAuto;
+        private System.Windows.Forms.Button disableAuto;
+        private System.Windows.Forms.ComboBox clickCombobox;
+        private System.Windows.Forms.TextBox msTextbox;
+        private System.Windows.Forms.Label msLabel;
+        private System.Windows.Forms.Label clickCombo;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Threading;
+using System.Windows.Input;
 
 namespace Srak
 {
@@ -24,31 +27,169 @@ namespace Srak
         const int MOUSEEVENTF_LEFTDOWN = 0x0002;
         const int MOUSEEVENTF_LEFTUP = 0x0004;
 
+        bool isActive = false;
+
         public Srak()
         {
             InitializeComponent();
+            this.Load += Srak_Load;
+            this.KeyPreview = true;
+            this.KeyDown += Auto_Keydown;
+
+            KeyboardHook.Start();
+            KeyboardHook.KeyDown += KeyboardHook_KeyDown;
         }
 
-        private async void Srak_Load(object sender, EventArgs e)
+        private void KeyboardHook_KeyDown(object sender, KeyEventArgs e)
         {
-            int time = 0;
-            while (time < 10)
+            if (e.KeyCode == Keys.F6)
             {
-                await SimulerAppuiToucheAsync(Keys.B);
-                Thread.Sleep(1000);
-                time++;
+                ToggleAuto(sender, e);
             }
         }
-        static Task SimulerAppuiToucheAsync(Keys touche)
+
+        private void Auto_Keydown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F6)
+            {
+                ToggleAuto(sender, e);
+            }
+        }
+
+        private void Srak_Load(object sender, EventArgs e)
+        {
+            enableAuto.Click += ToggleAuto;
+            disableAuto.Click += ToggleAuto;
+            msTextbox.Text = "100";
+            clickCombobox.Items.AddRange(new object[] { "Left Button", "Right Button" });
+            clickCombobox.SelectedItem = 1;
+        }
+
+        private async void ToggleAuto(object sender, EventArgs e)
+        {
+            isActive = !isActive;
+            if (isActive)
+            {
+                enableAuto.Enabled = false;
+                disableAuto.Enabled = true;
+                await Task.Run(() =>
+                {
+                    while (isActive)
+                    {
+                        mouseBtn(Cursor.Position.X, Cursor.Position.Y, "left");
+                        int ms = int.Parse(msTextbox.Text);
+                        Thread.Sleep(ms);
+                    }
+                });
+            }
+            else if (!isActive)
+            {
+                enableAuto.Enabled = true;
+                disableAuto.Enabled = false;
+            }
+        }
+
+        public static class KeyboardHook
+        {
+            private static LowLevelKeyboardListener _listener;
+
+            public static event EventHandler<KeyEventArgs> KeyDown;
+
+            static KeyboardHook()
+            {
+                _listener = new LowLevelKeyboardListener();
+                _listener.OnKeyDown += (s, e) => KeyDown?.Invoke(s, e);
+            }
+
+            public static void Start()
+            {
+                _listener.HookKeyboard();
+            }
+
+            public static void Stop()
+            {
+                _listener.UnhookKeyboard();
+            }
+        }
+
+        public class LowLevelKeyboardListener
+        {
+            private const int WH_KEYBOARD_LL = 13;
+            private const int WM_KEYDOWN = 0x0100;
+
+            private HookProc _proc;
+            private IntPtr _hookID = IntPtr.Zero;
+
+            public event EventHandler<KeyEventArgs> OnKeyDown;
+
+            public LowLevelKeyboardListener()
+            {
+                _proc = HookCallback;
+            }
+
+            public void HookKeyboard()
+            {
+                _hookID = SetHook(_proc);
+            }
+
+            public void UnhookKeyboard()
+            {
+                UnhookWindowsHookEx(_hookID);
+            }
+
+            private IntPtr SetHook(HookProc proc)
+            {
+                using (ProcessModule module = Process.GetCurrentProcess().MainModule)
+                {
+                    return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                        GetModuleHandle(module.ModuleName), 0);
+                }
+            }
+
+            private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+            {
+                if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+                {
+                    int vkCode = Marshal.ReadInt32(lParam);
+                    OnKeyDown?.Invoke(this, new KeyEventArgs((Keys)vkCode));
+                }
+                return CallNextHookEx(_hookID, nCode, wParam, lParam);
+            }
+
+            #region DllImports
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            private static extern IntPtr SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hMod, uint dwThreadId);
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+            [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+            #endregion
+
+            #region Delegate
+
+            private delegate IntPtr HookProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+            #endregion
+        }
+
+        static Task keyboardBtn(Keys key)
         {
             return Task.Run(() =>
             {
-                keybd_event((byte)touche, 0, KEYEVENTF_KEYDOWN, UIntPtr.Zero);
-                keybd_event((byte)touche, 0, KEYEVENTF_KEYUP, UIntPtr.Zero);
+                keybd_event((byte)key, 0, KEYEVENTF_KEYDOWN, UIntPtr.Zero);
+                keybd_event((byte)key, 0, KEYEVENTF_KEYUP, UIntPtr.Zero);
             });
         }
 
-        static void SimulerClicSouris(int x, int y)
+        static void mouseBtn(int x, int y, string btn)
         {
             SetCursorPos(x, y);
             mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, UIntPtr.Zero);
