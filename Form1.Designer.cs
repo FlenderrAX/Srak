75447d18d05d49cd74b629fb972b5af84b0da761
Amine <aminossky1@gmail.com>
2024-01-24 06:20:30 +0100

    - Refactored code - Added frames as containers - Solved all bugs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,9 +31,14 @@
             this.enableAuto = new System.Windows.Forms.Button();
             this.disableAuto = new System.Windows.Forms.Button();
             this.clickCombobox = new System.Windows.Forms.ComboBox();
-            this.msTextbox = new System.Windows.Forms.TextBox();
             this.msLabel = new System.Windows.Forms.Label();
             this.clickCombo = new System.Windows.Forms.Label();
+            this.hotkeyCombobox = new System.Windows.Forms.ComboBox();
+            this.hotkeyLabel = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.msTextbox = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.msTextbox)).BeginInit();
             this.SuspendLayout();
             // 
             // enableAuto
@@ -58,22 +63,16 @@
             // clickCombobox
             // 
             this.clickCombobox.FormattingEnabled = true;
-            this.clickCombobox.Location = new System.Drawing.Point(105, 341);
+            this.clickCombobox.Location = new System.Drawing.Point(6, 158);
             this.clickCombobox.Name = "clickCombobox";
             this.clickCombobox.Size = new System.Drawing.Size(121, 21);
             this.clickCombobox.TabIndex = 2;
-            // 
-            // msTextbox
-            // 
-            this.msTextbox.Location = new System.Drawing.Point(321, 342);
-            this.msTextbox.Name = "msTextbox";
-            this.msTextbox.Size = new System.Drawing.Size(121, 20);
-            this.msTextbox.TabIndex = 3;
+            this.clickCombobox.SelectionChangeCommitted += new System.EventHandler(this.mouseBtn_ChangeCommited);
             // 
             // msLabel
             // 
             this.msLabel.AutoSize = true;
-            this.msLabel.Location = new System.Drawing.Point(318, 326);
+            this.msLabel.Location = new System.Drawing.Point(3, 89);
             this.msLabel.Name = "msLabel";
             this.msLabel.Size = new System.Drawing.Size(62, 13);
             this.msLabel.TabIndex = 4;
@@ -82,29 +81,68 @@
             // clickCombo
             // 
             this.clickCombo.AutoSize = true;
-            this.clickCombo.Location = new System.Drawing.Point(102, 325);
+            this.clickCombo.Location = new System.Drawing.Point(3, 142);
             this.clickCombo.Name = "clickCombo";
             this.clickCombo.Size = new System.Drawing.Size(30, 13);
             this.clickCombo.TabIndex = 5;
             this.clickCombo.Text = "Click";
+            // 
+            // hotkeyCombobox
+            // 
+            this.hotkeyCombobox.FormattingEnabled = true;
+            this.hotkeyCombobox.Location = new System.Drawing.Point(6, 45);
+            this.hotkeyCombobox.Name = "hotkeyCombobox";
+            this.hotkeyCombobox.Size = new System.Drawing.Size(121, 21);
+            this.hotkeyCombobox.TabIndex = 6;
+            this.hotkeyCombobox.SelectionChangeCommitted += new System.EventHandler(this.ChangedCommitted_Click);
+            // 
+            // hotkeyLabel
+            // 
+            this.hotkeyLabel.AutoSize = true;
+            this.hotkeyLabel.Location = new System.Drawing.Point(3, 29);
+            this.hotkeyLabel.Name = "hotkeyLabel";
+            this.hotkeyLabel.Size = new System.Drawing.Size(41, 13);
+            this.hotkeyLabel.TabIndex = 7;
+            this.hotkeyLabel.Text = "Hotkey";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.msTextbox);
+            this.groupBox1.Controls.Add(this.hotkeyCombobox);
+            this.groupBox1.Controls.Add(this.clickCombobox);
+            this.groupBox1.Controls.Add(this.msLabel);
+            this.groupBox1.Controls.Add(this.hotkeyLabel);
+            this.groupBox1.Controls.Add(this.clickCombo);
+            this.groupBox1.Location = new System.Drawing.Point(246, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 195);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // msTextbox
+            // 
+            this.msTextbox.Location = new System.Drawing.Point(7, 105);
+            this.msTextbox.Name = "msTextbox";
+            this.msTextbox.Size = new System.Drawing.Size(120, 20);
+            this.msTextbox.TabIndex = 8;
             // 
             // Srak
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(458, 450);
-            this.Controls.Add(this.clickCombo);
-            this.Controls.Add(this.msLabel);
-            this.Controls.Add(this.msTextbox);
-            this.Controls.Add(this.clickCombobox);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.disableAuto);
             this.Controls.Add(this.enableAuto);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "Srak";
             this.Text = "root";
             this.Load += new System.EventHandler(this.Srak_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.msTextbox)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -113,9 +151,12 @@
         private System.Windows.Forms.Button enableAuto;
         private System.Windows.Forms.Button disableAuto;
         private System.Windows.Forms.ComboBox clickCombobox;
-        private System.Windows.Forms.TextBox msTextbox;
         private System.Windows.Forms.Label msLabel;
         private System.Windows.Forms.Label clickCombo;
+        private System.Windows.Forms.ComboBox hotkeyCombobox;
+        private System.Windows.Forms.Label hotkeyLabel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown msTextbox;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Windows.Input;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
 
 namespace Srak
 {
@@ -20,21 +19,24 @@ namespace Srak
 
         [DllImport("user32.dll", SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
-        static extern bool mouse_event(uint dwFlags, int dx, int dy, uint dwData, UIntPtr dwExtraInfo);
+        static extern void mouse_event(uint dwFlags, int dx, int dy, uint dwData, UIntPtr dwExtraInfo);
 
         const int KEYEVENTF_KEYDOWN = 0x0001;
         const int KEYEVENTF_KEYUP = 0x0002;
         const int MOUSEEVENTF_LEFTDOWN = 0x0002;
         const int MOUSEEVENTF_LEFTUP = 0x0004;
+        const int MOUSEEVENTF_RIGHTUP = 0x0010;
+        const int MOUSEEVENTF_RIGHTDOWN = 0x0008;
 
         bool isActive = false;
+        Keys usrHotkey = Keys.None;
+        string usrBtn = "left";
 
         public Srak()
         {
             InitializeComponent();
             this.Load += Srak_Load;
-            this.KeyPreview = true;
-            this.KeyDown += Auto_Keydown;
+            this.FormClosing += Srak_FormClosing;
 
             KeyboardHook.Start();
             KeyboardHook.KeyDown += KeyboardHook_KeyDown;
@@ -42,50 +44,121 @@ namespace Srak
 
         private void KeyboardHook_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.F6)
+            if (e.KeyCode == usrHotkey)
             {
-                ToggleAuto(sender, e);
-            }
-        }
-
-        private void Auto_Keydown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.F6)
-            {
-                ToggleAuto(sender, e);
+                ToggleAuto();
             }
         }
 
         private void Srak_Load(object sender, EventArgs e)
         {
-            enableAuto.Click += ToggleAuto;
-            disableAuto.Click += ToggleAuto;
+            enableAuto.Click += (s, ev) => ToggleAuto();
+            disableAuto.Click += (s, ev) => ToggleAuto();
             msTextbox.Text = "100";
-            clickCombobox.Items.AddRange(new object[] { "Left Button", "Right Button" });
-            clickCombobox.SelectedItem = 1;
+
+            clickCombobox.Items.Clear();
+            clickCombobox.Items.AddRange(new[] { "Left Button", "Right Button" });
+            clickCombobox.SelectedIndex = 0;
+            clickCombobox.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            hotkeyCombobox.Items.Clear();
+            hotkeyCombobox.Items.AddRange(new[] { "F6", "SPACE", "K", "PageUP" });
+            hotkeyCombobox.SelectedIndex = 0;
+            hotkeyCombobox.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            groupBox1.Text = "Mouse";
+
+            hotkeyLabel.Location = new System.Drawing.Point(5, 20);
+            hotkeyCombobox.Location = new System.Drawing.Point(10, 40);
+
+            clickCombo.Location = new System.Drawing.Point(5, 85);
+            clickCombobox.Location = new System.Drawing.Point(10, 105);
+
+            msLabel.Location = new System.Drawing.Point(5, 145);
+            msTextbox.Location = new System.Drawing.Point(10, 165);
+
+            groupBox1.Controls.AddRange(new Control[] {hotkeyCombobox, hotkeyLabel, clickCombo, clickCombobox, msTextbox, msLabel});
         }
 
-        private async void ToggleAuto(object sender, EventArgs e)
+        private void Srak_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            KeyboardHook.Stop();
+        }
+
+        private void ChangedCommitted_Click(object sender, EventArgs e)
+        {
+            switch (hotkeyCombobox.SelectedItem.ToString())
+            {
+                case "F6":
+                    usrHotkey = Keys.F6;
+                    break;
+                case "SPACE":
+                    usrHotkey = Keys.Space;
+                    break;
+                case "K":
+                    usrHotkey = Keys.K;
+                    break;
+                case "PageUP":
+                    usrHotkey = Keys.PageUp;
+                    break;
+            }
+
+            enableAuto.Text = $"Enable Autoclick ({usrHotkey})";
+            disableAuto.Text = $"Disable Autoclick ({usrHotkey})";
+        }
+
+        private void mouseBtn_ChangeCommited(object sender, EventArgs e)
+        {
+            usrBtn = clickCombobox.SelectedItem.ToString().ToLower().Split(' ')[0];
+        }
+
+        private async void ToggleAuto()
         {
             isActive = !isActive;
             if (isActive)
             {
+                msTextbox.Enabled = false;
                 enableAuto.Enabled = false;
                 disableAuto.Enabled = true;
                 await Task.Run(() =>
                 {
                     while (isActive)
                     {
-                        mouseBtn(Cursor.Position.X, Cursor.Position.Y, "left");
-                        int ms = int.Parse(msTextbox.Text);
-                        Thread.Sleep(ms);
+                        if (!string.IsNullOrEmpty(usrBtn))
+                        {
+                            this.Invoke(new Action(() =>
+                            {
+                                PerformMouseClick(usrBtn, Cursor.Position.X, Cursor.Position.Y);
+                            }));
+
+                            int ms;
+                            if (int.TryParse(msTextbox.Text, out ms))
+                                Thread.Sleep(ms);
+                        }
                     }
                 });
             }
-            else if (!isActive)
+            else
             {
                 enableAuto.Enabled = true;
                 disableAuto.Enabled = false;
+                msTextbox.Enabled = true;
+            }
+        }
+
+        private static void PerformMouseClick(string button, int x, int y)
+        {
+            SetCursorPos(x, y);
+            switch (button)
+            {
+                case "left":
+                    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, UIntPtr.Zero);
+                    mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, UIntPtr.Zero);
+                    break;
+                case "right":
+                    mouse_event(MOUSEEVENTF_RIGHTDOWN, x, y, 0, UIntPtr.Zero);
+                    mouse_event(MOUSEEVENTF_RIGHTUP, x, y, 0, UIntPtr.Zero);
+                    break;
             }
         }
 
@@ -178,22 +251,6 @@ namespace Srak
             private delegate IntPtr HookProc(int nCode, IntPtr wParam, IntPtr lParam);
 
             #endregion
-        }
-
-        static Task keyboardBtn(Keys key)
-        {
-            return Task.Run(() =>
-            {
-                keybd_event((byte)key, 0, KEYEVENTF_KEYDOWN, UIntPtr.Zero);
-                keybd_event((byte)key, 0, KEYEVENTF_KEYUP, UIntPtr.Zero);
-            });
-        }
-
-        static void mouseBtn(int x, int y, string btn)
-        {
-            SetCursorPos(x, y);
-            mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, UIntPtr.Zero);
-            mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, UIntPtr.Zero);
         }
     }
 }
